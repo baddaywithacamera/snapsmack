ba56503f054fa9f83a1ae4d3052d34793cdc8538
Sean McCormick <baddaywithacamera@gmail.com>
2026-02-19 11:02:06 -0700

    Old admin footer had "legacy leash" code in it that allowed me to flip back and forth between current and old css files. deprecated and removed from this version as no longer needing the control in the admin interface.

--- core/admin-footer.php
+++ core/admin-footer.php
@@ -1,20 +1,27 @@
 <?php
 /**
  * SnapSmack - Universal Admin Footer
- * Version: 1.6 - Minimalist Split
+ * Version: 7.0 - Clean Geometry Build
+ * -------------------------------------------------------------------------
+ * - REMOVED: v6-refactor-console (The Leash)
+ * - REMOVED: Inline style blocks
+ * - FIXED: Clean structural exit
+ * -------------------------------------------------------------------------
  */
 ?>
 <footer id="admin-universal-footer">
     <div class="footer-left">
-        A re-imagining of <a href="https://en.wikipedia.org/wiki/Pixelpost" target="_blank">Pixelpost</a>, 
-        inspired by <a href="https://en.wikipedia.org/wiki/Greymatter_(software)" target="_blank">GreyMatter</a> 
-        from <a href="https://manmadeghost.com/" target="_blank">Noah Grey</a>.
+        A RE-IMAGINING OF <a href="https://en.wikipedia.org/wiki/Pixelpost" target="_blank">PIXELPOST</a>, 
+        INSPIRED BY <a href="https://en.wikipedia.org/wiki/Greymatter_(software)" target="_blank">GREYMATTER</a> 
+        FROM <a href="https://manmadeghost.com/" target="_blank">NOAH GREY</a>.
     </div>
 
     <div class="footer-right">
         SNAPSMACK ALPHA V0.5 &copy; <?php echo date("Y"); ?>
     </div>
 </footer>
+
+<script src="assets/js/hotkey-engine.js"></script>
 
 </body>
 </html>
