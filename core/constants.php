08782483ea4f15ece5d5ccc1ee9e07b61d145002
Sean McCormick <theschoolofhardnocks@gmail.com>
2026-06-24 13:30:02 -0600

    the-grid 1.3.28 + core 0.7.299: grid crop/zoom, split, white-line fix

--- core/constants.php
+++ core/constants.php
@@ -63,8 +63,8 @@ if (PHP_SAPI !== 'cli' && !headers_sent()) {
     header('Referrer-Policy: strict-origin-when-cross-origin');
 }
 
-define('SNAPSMACK_VERSION', 'Alpha 0.7.297');
-define('SNAPSMACK_VERSION_SHORT', '0.7.297');
+define('SNAPSMACK_VERSION', 'Alpha 0.7.299');
+define('SNAPSMACK_VERSION_SHORT', '0.7.299');
 define('SNAPSMACK_VERSION_CODENAME', 'Front Row');
 
 // --- VERSION COMPARISON ---
